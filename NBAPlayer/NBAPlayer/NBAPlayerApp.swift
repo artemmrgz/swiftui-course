44e3f86ab620ba25dab574b2b49b698dfc89fc20
Artem Marhaza <artemmrgz@gmail.com>
2023-10-15 17:55:16 +0200

    create NBA Player project

---
+++ NBAPlayer/NBAPlayer/NBAPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBAPlayerApp.swift
+//  NBAPlayer
+//
+//  Created by Artem Marhaza on 15/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct NBAPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
