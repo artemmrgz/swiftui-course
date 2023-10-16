64c982b324b845ec23179b4fd7bf7e8273733224
Artem Marhaza <artemmrgz@gmail.com>
2023-10-16 19:43:17 +0200

    app fix

--- NBAPlayer/NBAPlayer/PlayerDetail.swift
+++ NBAPlayer/NBAPlayer/PlayerDetail.swift
@@ -15,7 +15,7 @@ struct PlayerDetail: View {
         VStack {
             Image(player.team.imageName).resizable().aspectRatio(contentMode: .fit)
             Image(player.imageName).clipShape(Circle()).background(Circle().foregroundColor(.white)).overlay(Circle().stroke(Color.white, lineWidth: 4)).offset(x: 0, y: -90).padding(.bottom, -70).shadow(radius: 15)
-            Text(player.name).font(.system(size: 40)).fontWeight(.heavy)
+            Text(player.name).font(.system(size: 40)).fontWeight(.heavy).lineLimit(1).padding(.leading).padding(.trailing).minimumScaleFactor(0.5)
             StatText(statName: "Age", statValue: "\(player.age)")
             StatText(statName: "Height", statValue: player.height)
             StatText(statName: "Weight", statValue: "\(player.weight)lbs")
@@ -27,6 +27,10 @@ struct PlayerDetail: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        PlayerDetail(player: players[0])
+        Group {
+            PlayerDetail(player: players[2]).environment(\.dynamicTypeSize, .xxxLarge).previewDevice("iPhone 14 Pro Max")
+            PlayerDetail(player: players[2]).environment(\.dynamicTypeSize, .xSmall).previewDevice("iPhone 13 mini")
+            PlayerDetail(player: players[2]).environment(\.dynamicTypeSize, .xxxLarge).previewDevice("iPhone 13 mini")
+        }
     }
 }
