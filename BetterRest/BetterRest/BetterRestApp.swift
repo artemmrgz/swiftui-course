1acd4784006578391453ab51c932a0b70b3bd0fb
Artem Marhaza <artemmrgz@gmail.com>
2023-11-09 15:32:28 +0100

    add BetterRest app

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Artem Marhaza on 06/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
